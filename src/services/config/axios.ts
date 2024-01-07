fdd57fcc103c10112d9d4d49a91143e63d9976e8
Iman Hadizadeh <i.hadizadeh@pspexpress.com>
2024-01-07 20:52:21 +0330

    improvment

--- src/services/config/axios.ts
+++ src/services/config/axios.ts
@@ -16,7 +16,6 @@ axiosInstance.interceptors.response.use(
   (error) => {
     switch (error?.response?.status) {
       case 401:
-        // console.log("Bad Or Good");
         break;
 
       default:
@@ -26,34 +25,5 @@ axiosInstance.interceptors.response.use(
     return Promise.reject(error);
   }
 );
-// axiosInstance.interceptors.response.use(
-//     response => {
-//       return response
-//     },
-//     error => {
-//       switch (error?.response?.status) {
-//         case 401:
-//         //   removeCookie(config.authorizationCookieName)
-//         //   removeCookie(config.userInfoCookieName)
-//         //   Router.push(PageEnum.signIn)
-//           break
-
-//         case 404:
-//         //   Router.push(PageEnum.error404)
-//           break
-
-//         case 500:
-
-//         case 503:
-//         //   Router.push(PageEnum.error500)
-//           break
-
-//         default:
-//           break
-//       }
-
-//       return Promise.reject(error)
-//     }
-//   )
 
 export { axiosInstance };
